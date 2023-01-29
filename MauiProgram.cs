b919731a166d6e2db1a48f89bc8573bc1ec54254
ComradeGurfild <latysa123@gmail.com>
2023-01-29 14:13:56 +0100

    PageResolver

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -19,6 +19,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
 			});
 		builder.Services.AddSingleton<SaveHolder>();
+        builder.Services.AddSingleton<BasketHolder>();
 
         return builder.Build();
 	}
